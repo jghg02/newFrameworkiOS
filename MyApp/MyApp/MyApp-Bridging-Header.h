9176fcc75f7d3120a2f9a493bbf89cc6171781ff
Josue Hernandez <jghg.02@gmail.com>
2015-05-16 21:13:27 -0300

    Added Framework into Bridding-Header.

--- MyApp/MyApp/MyApp-Bridging-Header.h
+++ MyApp/MyApp/MyApp-Bridging-Header.h
@@ -2,3 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//IMPORTANT: to import framework to use in Swift is nedded add this line in this file.
+#import <FutureFramework/FutureFramework.h>
