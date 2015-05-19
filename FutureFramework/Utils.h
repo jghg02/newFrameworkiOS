4d2474fb09e426af7c08194ca4e55f93f702445b
Josue Hernandez <jghg.02@gmail.com>
2015-05-19 20:58:36 -0300

    Added Utils class into Framework to set Layouts into UIVIew

---
+++ FutureFramework/Utils.h
@@ -0,0 +1,13 @@
+//
+//  Utils.h
+//  FutureFramework
+//
+//  Created by Josue Hernandez on 5/19/15.
+//  Copyright (c) 2015 Josue Hernandez. All rights reserved.
+//
+
+#ifndef FutureFramework_Utils_h
+#define FutureFramework_Utils_h
+
+
+#endif
---
+++ FutureFramework/Utils.m
@@ -0,0 +1,9 @@
+//
+//  Utils.m
+//  FutureFramework
+//
+//  Created by Josue Hernandez on 5/19/15.
+//  Copyright (c) 2015 Josue Hernandez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
