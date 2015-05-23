2b52ef712f42f9da20ca6e6b9ff43cb049596b2b
Josue Hernandez <jghg.02@gmail.com>
2015-05-23 18:32:07 -0300

    Added a new ViewController into Framework...

---
+++ FutureFramework/LoaderViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  FutureFramework
+//
+//  Created by Josue Hernandez on 5/23/15.
+//  Copyright (c) 2015 Josue Hernandez. All rights reserved.
+//
+
+#import "LoaderViewController.h"
+
+@interface LoaderViewController ()
+
+@end
+
+@implementation LoaderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
